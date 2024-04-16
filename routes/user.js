6aff85fa3da8f7de46cebd417bf77a788c24452d
aldotp <aldo2001aldo@gmail.com>
2024-04-16 16:01:21 +0700

    first commit

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/", getUsers);
+router.get("/", auth, getUsers);
 router.get("/profile", auth, getProfile);
 
 module.exports = router;
